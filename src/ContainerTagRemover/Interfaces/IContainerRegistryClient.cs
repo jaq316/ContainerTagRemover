42bd69843bc77ec6777b49166c3102a3445fa288
Jacques Snyman <jacques@jacquessnyman.co.za>
2024-12-04 16:31:07 +0200

    Add `CancellationToken` parameter to `IContainerRegistryClient` and `DockerhubClient`
    * **DockerhubClient.cs**
      - Add `using System.Threading;` directive.
    * **IContainerRegistryClient.cs**
      - Add `using System.Threading;` directive.

--- src/ContainerTagRemover/Interfaces/IContainerRegistryClient.cs
+++ src/ContainerTagRemover/Interfaces/IContainerRegistryClient.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace ContainerTagRemover.Interfaces
 {
--- src/ContainerTagRemover/Services/DockerhubClient.cs
+++ src/ContainerTagRemover/Services/DockerhubClient.cs
@@ -4,6 +4,7 @@ using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
 using ContainerTagRemover.Interfaces;
+using System.Threading;
 
 namespace ContainerTagRemover.Services
 {
